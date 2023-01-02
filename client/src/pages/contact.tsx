891e398a8609de5d820789c19da38f514f4ec08d
Tejas Deore <tejasdeore80@gmail.com>
2023-01-02 15:45:23 +0530

    updated contact.tsx

--- client/src/pages/contact.tsx
+++ client/src/pages/contact.tsx
@@ -5,7 +5,10 @@ const contact = () => {
   return (
     <>
     <Head>Contact - Strimit</Head>
-      <div>Our team will contact you shortly</div>
+      <div>
+        <h1 className="text-center text-4xl font-bold">Our team will contact you shortly</h1>
+        <h1 className="text-center text-4xl font-bold">Please look out for a mail from team@strimit.in</h1>
+      </div>
     </>
   );
 }
