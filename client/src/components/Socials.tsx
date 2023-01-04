5dc3fd9d0035a8ddb3e5a2de2c478ba9daab4020
Tejas Deore <tejasdeore80@gmail.com>
2023-01-04 17:32:30 +0530

    added width and height to social icons

--- client/src/components/Socials.tsx
+++ client/src/components/Socials.tsx
@@ -5,9 +5,9 @@ const Socials = () => {
 
     return (
         <div className="space-x-10 bg-theme-black bg-fuchsia-50 h-10 flex items-center justify-center text-center">
-            <a href = "https://www.instagram.com/_strimit/" target="_blank"><p><BsInstagram></BsInstagram></p></a>
-            <a href = "https://www.facebook.com/people/_strimit/100089197329228/?mibextid=ZbWKwL" target="_blank"><p><BsFacebook></BsFacebook></p></a>
-            <a href = "https://twitter.com/_strimit?t=bY4bUx8qmG06uFYyu-yIlw&s=08" target="_blank"><p><BsTwitter></BsTwitter></p></a>
+            <a href = "https://www.instagram.com/_strimit/" target="_blank"><p><BsInstagram className="w-5 h-5"></BsInstagram></p></a>
+            <a href = "https://www.facebook.com/people/_strimit/100089197329228/?mibextid=ZbWKwL" target="_blank"><p><BsFacebook className="w-5 h-5"></BsFacebook></p></a>
+            <a href = "https://twitter.com/_strimit?t=bY4bUx8qmG06uFYyu-yIlw&s=08" target="_blank"><p><BsTwitter className="w-5 h-5"></BsTwitter></p></a>
         </div>
     );
 }
